b3ef1f5b24c03d951d15d6a7a8cb3b9cee0cb5d5
julia <zhydanovajulia@gmail.com>
2011-10-28 16:34:57 +0300

    Initial commit

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '0a786947f070bb9919c291fde066eca097d0f9ffeff9b7ca44ac03f53fa3d52a5ca54575b50a3554eb8fb8d4eb59b841bda55721329e22e38c9c24aaa1b3162b'
---
+++ demo_forum/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoForum::Application.config.secret_token = '61e4fc3a6881bbed87e5e03eb2102ceef0ee350caacef0fb5c996168e34c9b25169653f26096e119f2b0d5eea910e6d37201e9a8b776a32423a405eaf0e81b88'
